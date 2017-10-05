41884f578cc8952396eae808c99992321b1073c9
Nazmuz Shakib Pranto <npranto@gmail.com>
2017-10-05 11:14:26 -0400

    completes drum-kit mini project

--- drum-kit/src/index.js
+++ drum-kit/src/index.js
@@ -1,3 +1,12 @@
-module.exports = {
-	
+function run () {
+	window.addEventListener('keydown', function(e) {
+		const audioElement = document.querySelector(`.keypad-audio[data-key="${e.keyCode}"]`);
+		if (audioElement) {
+			document.querySelector(`.key[data-key="${e.keyCode}"]`).classList.add('playing');
+			audioElement.currentTime = 0;
+			audioElement.play();
+		}
+	})
 }
+
+run();
