d685995c7b3ddb9ae424ee4f6c5692b9ac41d420
Nazmuz Shakib Pranto <npranto@gmail.com>
2017-10-05 16:39:48 -0400

    completes drum-kit project

---
+++ drum-kit/README.md
@@ -0,0 +1,5 @@
+# Drum Kit
+
+A simple drum kit to play different sounds, written in vanilla javascript
+
+[Try it out!](www.google.com)
--- drum-kit/src/index.js
+++ drum-kit/src/index.js
@@ -1,11 +1,22 @@
 function run () {
+	// adds playing effect on keydown
 	window.addEventListener('keydown', function(e) {
 		const audioElement = document.querySelector(`.keypad-audio[data-key="${e.keyCode}"]`);
+		const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
 		if (audioElement) {
-			document.querySelector(`.key[data-key="${e.keyCode}"]`).classList.add('playing');
+			key.classList.add('playing');
 			audioElement.currentTime = 0;
 			audioElement.play();
 		}
+	})
+
+	// removes playing effects from each key after each transition
+	const keys = document.querySelectorAll('.key');
+	console.log(keys);
+	keys.forEach(function (key, index, keys) {
+		key.addEventListener('transitionend', function (e) {
+			key.classList.remove('playing');
+		})
 	})
 }
 
